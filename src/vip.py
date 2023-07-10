2607a83c2da70923dc10e77e7a21f4f043098a66
VILA Gael <gael.vila@creatis.insa-lyon.fr>
2023-07-10 15:49:06 +0200

    stable connexion

--- src/vip.py
+++ src/vip.py
@@ -4,16 +4,28 @@ from os.path import join, dirname, exists
 
 ########################### VARIABLES & ERRORS ################################
 # -----------------------------------------------------------------------------
+# API URL
 __PREFIX = "https://vip.creatis.insa-lyon.fr/rest/"
+
+# API key
 __apikey = None
 __headers = {'apikey': __apikey}
 
-path = join(dirname(__file__), 'certif.crt')
-if not exists(path):
-    __certif = None
-else:
-    __certif = path
-del path
+# Requests session
+session = requests.Session()
+
+# Strategy for retrying
+retry_strategy = requests.adapters.Retry(
+    total = 4, # Retry 3 times at most
+    status_forcelist  = [ 
+        104,    # ConnectionResetError ?
+        500,    # Internal Server Error
+        502,    # Bad Gateway ou Proxy Error
+        503,    # Service Unavailable
+        504     # Gateway Time-out
+    ],
+    backoff_factor = 8 # retries after 0s, 16s, 32s, 64s
+)
 
 # -----------------------------------------------------------------------------
 def setApiKey(value)->bool:
@@ -25,26 +37,25 @@ def setApiKey(value)->bool:
     head_test = {
                  'apikey': value,
                 }
-    rq = requests.put(url, headers=head_test, verify=__certif)
+    # Send a test request
+    rq = requests.put(url, headers=head_test)
     res = detect_errors(rq)
     if res[0]:
+        # Error
         if res[1] == 40101:
             return False
         else:
             raise RuntimeError("Error {} from VIP : {}".format(res[1], res[2]))
     else:
-        global __apikey
+        # OK
+        global __apikey, session
+        # Set the API key
         __apikey = value
         __headers['apikey'] = value
+        # Create a new Requests session
+        session = requests.Session()
+        session.mount(__PREFIX, requests.adapters.HTTPAdapter(max_retries=retry_strategy))
         return True
-
-# -----------------------------------------------------------------------------
-def setCertifPath(path)->bool:
-    """
-    TODO : verify if the certif work
-    """
-    __certif = path
-    return True
 
 # -----------------------------------------------------------------------------
 def detect_errors(req)->tuple:
@@ -83,7 +94,7 @@ def create_dir(path)->bool:
     Return True if done, False otherwise
     """
     url = __PREFIX + 'path' + path
-    rq = requests.put(url, headers=__headers, verify=__certif)
+    rq = session.put(url, headers=__headers)
     try:
         manage_errors(rq)
     except RuntimeError:
@@ -116,7 +127,7 @@ def _path_action(path, action) -> requests.models.Response:
     """
     assert action in ['list', 'exists', 'properties', 'md5']
     url = __PREFIX + 'path' + path + '?action=' + action
-    rq = requests.get(url, headers=__headers, verify=__certif)
+    rq = session.get(url, headers=__headers)
     manage_errors(rq)
     return rq
 
@@ -153,7 +164,7 @@ def delete_path(path)->bool:
     Return True if done, False otherwise
     """
     url = __PREFIX + 'path' + path
-    rq = requests.delete(url, headers=__headers, verify=__certif)
+    rq = session.delete(url, headers=__headers)
     try:
         manage_errors(rq)
     except RuntimeError:
@@ -177,7 +188,7 @@ def upload(path, where_to_save)->bool:
               }
     with open(path, 'rb') as fid:
         data = fid.read()
-    rq = requests.put(url, headers=headers, data=data, verify=__certif)
+    rq = session.put(url, headers=headers, data=data)
     try:
         manage_errors(rq)
     except RuntimeError:
@@ -193,7 +204,7 @@ def download(path, where_to_save):
         - where_to_save : on local computer
     """
     url = __PREFIX + 'path' + path + '?action=content'
-    rq = requests.get(url, headers=__headers, stream=True, verify=__certif)
+    rq = session.get(url, headers=__headers, stream=True)
     try:
         manage_errors(rq)
     except RuntimeError:
@@ -207,14 +218,14 @@ def download(path, where_to_save):
 # -----------------------------------------------------------------------------
 def list_executions()->list:
     url = __PREFIX + 'executions'
-    rq = requests.get(url, headers=__headers, verify=__certif)
+    rq = session.get(url, headers=__headers)
     manage_errors(rq)
     return rq.json()
 
 # -----------------------------------------------------------------------------
 def count_executions()->int:
     url = __PREFIX + 'executions/count'
-    rq = requests.get(url, headers=__headers, verify=__certif)
+    rq = session.get(url, headers=__headers)
     manage_errors(rq)
     return int(rq.text)
 
@@ -231,14 +242,14 @@ def init_exec(pipeline, name="default", inputValues={}, resultsLocation="/vip/Ho
             "inputValues": inputValues,
             "resultsLocation": resultsLocation
            }
-    rq = requests.post(url, headers=headers, json=data_, verify=__certif)
+    rq = session.post(url, headers=headers, json=data_)
     manage_errors(rq)
     return rq.json()["identifier"]
 
 # -----------------------------------------------------------------------------
 def execution_info(id_exec)->dict:
     url = __PREFIX + 'executions/' + id_exec
-    rq = requests.get(url, headers=__headers, verify=__certif)
+    rq = session.get(url, headers=__headers)
     manage_errors(rq)
     return rq.json()
 
@@ -250,21 +261,21 @@ def is_running(id_exec)->bool:
 # -----------------------------------------------------------------------------
 def get_exec_stderr(exec_id) -> str:
     url = __PREFIX + 'executions/' + exec_id + '/stderr'
-    rq = requests.get(url, headers=__headers, verify=__certif)
+    rq = session.get(url, headers=__headers)
     manage_errors(rq)
     return rq.text
 
 # -----------------------------------------------------------------------------
 def get_exec_stdout(exec_id) -> str:
     url = __PREFIX + 'executions/' + exec_id + '/stdout'
-    rq = requests.get(url, headers=__headers, verify=__certif)
+    rq = session.get(url, headers=__headers)
     manage_errors(rq)
     return rq.text
 
 # -----------------------------------------------------------------------------
 def get_exec_results(exec_id) -> str:
     url = __PREFIX + 'executions/' + exec_id + '/results'
-    rq = requests.get(url, headers=__headers, verify=__certif)
+    rq = session.get(url, headers=__headers)
     manage_errors(rq)
     return rq.json()
 
@@ -273,7 +284,7 @@ def kill_execution(exec_id, deleteFiles=False) -> bool:
     url = __PREFIX + 'executions/' + exec_id
     if deleteFiles:
         url += '?deleteFiles=true'
-    rq = requests.delete(url, headers=__headers, verify=__certif)
+    rq = session.delete(url, headers=__headers)
     try:
         manage_errors(rq)
     except RuntimeError:
@@ -285,14 +296,14 @@ def kill_execution(exec_id, deleteFiles=False) -> bool:
 # -----------------------------------------------------------------------------
 def list_pipeline()->list:
     url = __PREFIX + 'pipelines'
-    rq = requests.get(url, headers=__headers, verify=__certif)
+    rq = session.get(url, headers=__headers)
     manage_errors(rq)
     return rq.json()
 
 # -----------------------------------------------------------------------------
 def pipeline_def(pip_id)->dict:
     url = __PREFIX + 'pipelines/' + pip_id
-    rq = requests.get(url, headers=__headers, verify=__certif)
+    rq = session.get(url, headers=__headers)
     manage_errors(rq)
     return rq.json()
 
@@ -300,7 +311,7 @@ def pipeline_def(pip_id)->dict:
 # -----------------------------------------------------------------------------
 def platform_info()->dict:
     url = __PREFIX + 'platform'
-    rq = requests.get(url, headers=__headers, verify=__certif)
+    rq = session.get(url, headers=__headers)
     manage_errors(rq)
     return rq.json()
 
@@ -318,7 +329,7 @@ def get_apikey(username, password)->str:
             "username": username, 
             "password": password
            }
-    rq = requests.post(url, headers=headers, json=data_, verify=__certif)
+    rq = session.post(url, headers=headers, json=data_)
     manage_errors(rq)
     return rq.json()['httpHeaderValue']
 
