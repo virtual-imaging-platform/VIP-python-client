19f18152c249ac1fdcb4a1f9dd1717ef6d0cdc68
cmouton <claire.mouton@creatis.insa-lyon.fr>
2023-07-13 11:43:40 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+setup(
+    name='vip-python-client',            # The name registered on PyPI
+    version='0.0.1',
+    description='Use VIP through a Python client.', # This text will be displayed on the plugin page
+    include_package_data=True,
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=['requests'],      # Add any plugin dependencies here
+)
