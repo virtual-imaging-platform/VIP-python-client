5c9d48e2f14643bb7780981e433fa47b0682feba
Axel Bonnet <axel.bonnet@creatis.insa-lyon.fr>
2025-06-11 18:35:33 +0200

    new release 0.1.9

--- src/vip_client/__init__.py
+++ src/vip_client/__init__.py
@@ -8,7 +8,7 @@ For more information: https://github.com/virtual-imaging-platform/VIP-python-cli
 """
 
 # Informations
-__version__ = "0.1.9-a1"
+__version__ = "0.1.9"
 __license__ = "CECILL-B"
 
 from vip_client.classes import VipSession
